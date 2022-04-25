aac354f006e971631ffc215a4df3957372ef0b97
Eros Linnyker <linnykereros@gmail.com>
2022-04-25 15:26:22 -0300

    message

--- src/server.js
+++ src/server.js
@@ -10,12 +10,11 @@ const connectToDatabase = require("./database");
 connectToDatabase();
 
 const app = express();
-const port = 5500;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Backend started at http://localhost:${port}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Backend started at http://localhost:`);
 });
